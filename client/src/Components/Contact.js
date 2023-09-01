e3b93022aa8de7a89a6e57cd9433ca6c03b0f684
tlam2139 <tlam2139@uni.sydney.edu.au>
2023-09-01 15:25:45 +1000

    contacts page amendment

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className='container'>
         <h1>Contact</h1>
-        <h1>Dr.Slade Matthews</h1>
+        <p>Dr.Slade Matthews</p>
     </div>
   )
 }
