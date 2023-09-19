b4b6aa1551b24ac414c5ac62344777469eee86cf
William Anthony Widyadharma <wwid6807@uni.sydney.edu.au>
2023-09-20 00:48:15 +1000

    added links to github on footer and contact page

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import githublogo from '../assets/images/github_icon.png'
+
 const Footer = () => {
   return (
     <section className="nearlyEnd">
-        <p>By <Link to="/contact" className="end"><i>Dr.Slade Matthews</i></Link></p>
-
-      </section>
+        <Link to='https://github.com/sladem-tox?tab=repositories'><img align='left' src={githublogo} style={{ width: 30, height: 30, paddingTop: 0}}></img></Link>
+        <p style={{paddingTop: 20}}>By <Link to="/contact" className="end" ><i>Dr.Slade Matthews</i></Link></p>
+    </section>
   )
 }
 
