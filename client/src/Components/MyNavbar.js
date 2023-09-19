d8e76017456c52840703ec27fad479084404a9ee
andrew <aadi5934@uni.sydney.edu.au>
2023-09-19 11:29:30 +1000

    added decorations on formula page

--- client/src/Components/MyNavbar.js
+++ client/src/Components/MyNavbar.js
@@ -29,28 +29,6 @@ function MyNavbar() {
 
 
   return (
-    // If route == default don't render
-
-    // <Navbar bg="light" expand="lg" className='bg-blue'>
-    //   <div className='container'>
-    //     <Navbar.Brand as={Link} to="/pharmakinetics">pk_calcs</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="ml-auto">
-    //         <Nav.Link as={Link} to="/pharmakinetics">Home</Nav.Link>
-    //         <Nav.Link as={Link} to="/about">About</Nav.Link>
-    //         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-    //         <NavDropdown title="Formula" id="basic-nav-dropdown">
-    //           {links.map(link => (
-    //             <NavDropdown.Item as={Link} to={`/formula/${link.url}`} key={link.id}>
-    //               {link.formula_name}
-    //             </NavDropdown.Item>
-    //           ))}
-    //         </NavDropdown>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </div>
-    // </Navbar>
     <div>
       <header className="header2">
         <nav className="navBar">
