0d7b53d906321a392497ecd8e84d6bdc333506c7
Yan  Gunawan <yangunawan@yans-mbp.student.wireless.sydney.edu.au>
2023-09-22 15:22:13 +1000

    edited footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import githublogo from '../assets/images/github_icon.png'
 import logo from '../assets/images/USYD_LOGO_WHITE.png';
+
+
+
 const Footer = () => {
   return (
     <section className="nearlyEnd footer">
@@ -11,34 +14,34 @@ const Footer = () => {
           <div className="col-lg-3 col-md-6">
             <h5 className="text-uppercase">Quick Links</h5>
             <ul className="list-unstyled mb-0">
-              <li><Link className='text-dark' to="/pharmacokinetics">Home</Link></li>
-              <li><Link className='text-dark' to= "/about">About</Link></li>
-              <li><Link className='text-dark' to="/contact">Contact</Link></li>
+              <li><Link className='text-white' to="/pharmacokinetics">Home</Link></li>
+              <li><Link className='text-white' to= "/about">About</Link></li>
+              <li><Link className='text-white' to="/contact">Contact</Link></li>
 
-              <li><Link to = "https://github.com/sladem-tox" className='text-dark'> Dr. Slade Matthews's Github</Link></li>
+              <li><Link to = "https://github.com/sladem-tox" className='text-white'> Dr. Slade Matthews's Github</Link></li>
 
             </ul>
           </div>
           <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">About the Author</h5>
-            <Link to = "https://www.sydney.edu.au/medicine-health/about/our-people/academic-staff/slade-matthews.html#collapseprofileprojects"className='text-dark'>More About Dr. Slade Matthews</Link>
+            <Link to = "https://www.sydney.edu.au/medicine-health/about/our-people/academic-staff/slade-matthews.html#collapseprofileprojects"className='text-white'>More About Dr. Slade Matthews</Link>
 
           </div>
 
           <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">Contact & Support</h5>
             <ul className="list-unstyled mb-0">
-              <li><a href="cs.reception@sydney.edu.au" class="text-dark">Email Address</a></li>
-              <li><a href="tel:+61 2 9351 2222" class="text-dark">Phone</a></li>
+              <li><a href="cs.reception@sydney.edu.au" class="text-white">Email Address</a></li>
+              <li><a href="tel:+61 2 9351 2222" class="text-white">Phone</a></li>
 
             </ul>
           </div>
           <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">Legal</h5>
             <ul className="list-unstyled mb-0">
-              <li><Link className='text-dark'>Privacy Policy</Link></li>
-              <li><Link className='text-dark'>Terms Of Use</Link></li>
-              <li><Link className='text-dark'>Disclaimer</Link></li>
+              <li><Link className='text-white'>Privacy Policy</Link></li>
+              <li><Link className='text-white'>Terms Of Use</Link></li>
+              <li><Link className='text-white'>Disclaimer</Link></li>
             </ul>
           </div>
 
