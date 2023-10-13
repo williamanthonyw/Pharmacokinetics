c47e697afcdd55bc4e99a211f1078ad18141abe7
Yan  Gunawan <yangunawan@yans-mbp.student.wireless.sydney.edu.au>
2023-10-13 14:28:28 +1100

    fix missing formula content

--- client/src/Components/ReferenceFormula.js
+++ client/src/Components/ReferenceFormula.js
@@ -2,6 +2,7 @@
 import React, {useEffect} from 'react'
 import Latex from './Latex'
 import mydata from './data'
+import triangle from '../assets/images/Pharmacokinetic_Triangle.png';
 
 
 const ReferenceFormula = () => {
@@ -21,6 +22,28 @@ const ReferenceFormula = () => {
                     </h3>
                 ))},
             </ul>
+
+            <h1 style={{ color: '#e54726' }}>Constants: Important Equations in Pharmacology</h1>
+            <br></br>
+            <p><b>Volume of distribution (Vd): <Latex latex_code="A_b = V_d * C_p"></Latex> or </b><Latex latex_code="V_d = \frac{A_b}{C_p}"></Latex></p>
+            <br></br>  
+            <p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Latex latex_code="Loading\:dose = V_d * C_p \tiny(desired)"></Latex></p>
+            <br></br>
+            <p><b>Clearance (Cl): <Latex latex_code="RE = Cl * C_p"></Latex> or </b><Latex latex_code="Cl = \frac{RE}{C_p}"></Latex></p>
+            <br></br>
+            <p>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Latex latex_code="Maintenance\:dose = Cl * C_p"></Latex></p>
+            <br></br>
+            <p><b>The half life ( <Latex latex_code="t_\frac{1}{2}"></Latex> ):</b> <Latex latex_code="t_\frac{1}{2} = \frac{0.693V_d}{Cl}"></Latex></p>
+            <br></br>
+            <p><b>Fractional oral availability (F):</b> <Latex latex_code="F = \frac{AUC_{po}}{AUC_{iv}}"></Latex></p>
+            <br></br>
+            <p><b>Creatinine clearance (CrCl):</b> <Latex latex_code="CrCl\:(mL/s) = \frac{(140-age)\:*\:weight[kg]}{50000\:*\:[creatinine](*\:0.85\:for\:women)}"></Latex></p>
+            <br></br>
+            <p><b>Dose adjustment in renal failure:</b> <Latex latex_code="\frac{dose\:rate_{patient}}{*dose\:rate_{normal}} = \frac{CrCl_{patient}}{Crcl_{normal}}"></Latex></p>
+            <br></br>
+            <p><b>Dose adjustment in renal failure when fu &lt; I:</b> <Latex latex_code="\frac{dose\:rate_{patient}}{*dose\:rate_{normal}} = (I\:-\:fu)\:+\:fu\:\times\:\frac{(CrCl_{patient})}{(CrCl_{normal})}"></Latex></p>
+            <br></br>
+            <img src={triangle} alt="Pharmacokinetic Triangle" height={500} width={500}/>
         </div>
     )
 };
