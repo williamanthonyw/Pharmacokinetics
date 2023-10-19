23a02ba2f784c50a025605622cb96413386b49f0
andrew <aadi5934@uni.sydney.edu.au>
2023-10-19 14:50:01 +1100

    formula definitions

--- client/src/Components/Latex.js
+++ client/src/Components/Latex.js
@@ -1,15 +1,14 @@
 import { render } from '@testing-library/react';
 import React, { useEffect, useRef } from 'react'
 import katex from 'katex';
-
-const Latex = ({latex_code}) => {
+const Latex = ({ latex_code }) => {
     const nodeRef = useRef();
 
     useEffect(() => {
         katex.render(latex_code, nodeRef.current);
     }, [latex_code]);
 
-    return <div className='btn btn-primary 'ref={nodeRef}/>
+    return <div className='btn btn-primary ' ref={nodeRef} title={"Click to copy formula\nVd = Volume of distribution\nK = red"} />
 }
 
 
