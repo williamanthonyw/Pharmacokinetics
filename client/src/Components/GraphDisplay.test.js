b1c1e43d98d1e951c0a10d8367fbc2ee73a11443
andrew <aadi5934@uni.sydney.edu.au>
2023-10-21 15:39:27 +1100

    graph display unit test

--- client/src/Components/GraphDisplay.test.js
+++ client/src/Components/GraphDisplay.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import GraphDisplay from './GraphDisplay';
-
+import mydata from './data';
 test('GraphDisplay renders without crashing', () => {
   render(<GraphDisplay variables={{}} equation="x^2" isMulipleOralDosing={false} is_testing={true} />);
   expect(screen.getByText("Plasma Concentration Over Time")).toBeInTheDocument();
@@ -10,6 +10,18 @@ test('Test multiple oral dosing renders without crashing', () => {
   render(<GraphDisplay variables={{}} equation="x^2" isMulipleOralDosing={true} is_testing={true} />);
   expect(screen.getByText("Plasma Concentration Over Time")).toBeInTheDocument();
 });
+test('test variable renders without crashing', () => {
+  let variables = mydata[0].data.variables;
+  render(<GraphDisplay variables={variables} equation="x^2" isMulipleOralDosing={false} is_testing={true} />);
+ 
+});
+test('test variable renders without crashing', () => {
+  let variables = mydata[2].data.variables;
+  let equation = mydata[2].data.equation
+  render(<GraphDisplay variables={variables} equation={equation} isMulipleOralDosing={false} is_testing={true} />);
+ 
+});
+
 
 // test('GraphDisplay renders chart with single equation', () => {
 //   render(<GraphDisplay variables={{}} equation="x^2" isMulipleOralDosing={false} is_testing={true} />);
