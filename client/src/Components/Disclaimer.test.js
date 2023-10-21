08869b53d34d51759cc6ebc7f25efa00ac818e70
andrew <aadi5934@uni.sydney.edu.au>
2023-10-21 15:42:28 +1100

    testing reference formula renders

--- client/src/Components/Disclaimer.test.js
+++ client/src/Components/Disclaimer.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Disclaimer from './Disclaimer';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import ReferenceFormula from './ReferenceFormula';
 
 test('renders the disclaimer title', () => {
     render(
@@ -9,7 +10,7 @@ test('renders the disclaimer title', () => {
             <App />
         </MemoryRouter>);
     expect(screen.getByText(/If you require any more information or have any questions about our site's disclaimer, please feel free to contact us by email at/i)).toBeInTheDocument();
-   
+
 });
 
 test('renders the consent section', () => {
@@ -17,14 +18,14 @@ test('renders the consent section', () => {
         <App />
     </MemoryRouter>);
     expect(screen.getByText("Should we update, amend or make any changes to this document, those changes will be prominently posted here.")).toBeInTheDocument()
-   
+
 });
 
 test('renders the update section', () => {
-   render(<MemoryRouter initialEntries={["", "/disclaimer"]}>
+    render(<MemoryRouter initialEntries={["", "/disclaimer"]}>
         <App />
     </MemoryRouter>);
-   expect(screen.getByText("Update")).toBeInTheDocument()
+    expect(screen.getByText("Update")).toBeInTheDocument()
 });
 
 test('renders the USYD disclaimer link', () => {
@@ -33,4 +34,10 @@ test('renders the USYD disclaimer link', () => {
     </MemoryRouter>);
     expect(screen.getByText("By using our website, you hereby consent to our disclaimer and agree to its terms.")).toBeInTheDocument()
     // expect(linkElement).toHaveAttribute('href', 'https://www.sydney.edu.au/disclaimer.html');
+});
+
+test("reference formula", () => {
+    render(
+        <ReferenceFormula />
+    );
 });
